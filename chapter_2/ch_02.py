c6abd0537284b72fc74eb24f5da821ce0df31b38
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-05-27 19:31:23 -0400

    Add files via upload

--- chapter_2/ch_02.py
+++ chapter_2/ch_02.py
@@ -6,3 +6,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
+# # implementing a perceptron learning algo in python
+
+# # an object-oriented perceptiron API
+
+
+class Perceptron(object):
+    """ Perceptron classifier
+    
+    Parameters 
+    ------------
+    etea : float
+        Learning rate (between 0.0 and 1.0)
+    n_iter : int
+        Passes over the training dataset
+    random_state : int
+        Random number generator seed for random weight
+        initialization.
+
+    Attributes
+    ------------
+    w_ " 1d-array
+        Weights after fitting
+    errors_ " list
+        Number of misclassifications (updates) in each epoch
+    
+    """
+
