15207d1590da9a09a807c2595a126af0c331a52b
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-05-31 18:30:26 -0400

    Add files via upload

--- chapter_2/ch_02.py
+++ chapter_2/ch_02.py
@@ -101,3 +101,27 @@ df.tail()
 df = pd.read_csv('../iris.data', 
                 header=None, encoding='utf-8')
 df.tail()
+
+# ### plotting the iris data
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosaa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+plt.savefig('02_06.png', dpi=300)
+plt.show()
+
+
