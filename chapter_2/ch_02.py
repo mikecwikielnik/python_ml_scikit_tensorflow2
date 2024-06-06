7b1602c4addf6b4a860ed4a7ed909594fef816c5
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-06 13:57:48 -0400

    Add files via upload

--- chapter_2/ch_02.py
+++ chapter_2/ch_02.py
@@ -264,6 +264,24 @@ class AdalineGD(object):
         """Return class label after unit step"""
         return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
 
+fix, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(sum squared error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum Squared Error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.savefig('02_11.png', dpi=300)
+plt.show()
+
+
 
 
 
