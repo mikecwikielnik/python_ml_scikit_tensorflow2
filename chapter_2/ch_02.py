25e503d3c2b6bf843680e00606adc23a4ed779fc
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-06 13:15:38 -0400

    Add files via upload

--- chapter_2/ch_02.py
+++ chapter_2/ch_02.py
@@ -80,7 +80,7 @@ class Perceptron(object):
 
 v1 = np.array([1, 2, 3])
 v2 = 0.5 * v1
-np.across(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+np.arccos(v1.dot(v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
 
 # training a perceptron model on the iris dataset
 
@@ -137,4 +137,136 @@ plt.ylabel('Number of updates')
 
 # plt.savefig('02_07.png', dpi=300)
 plt.show()
+
+# ### A function for plotting decision regions
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha = 0.8,
+                    c=colors[idx],
+                    marker = markers[idx],
+                    label = cl,
+                    edgecolor = 'black')
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+# plt.savefig('02_08.png', dpi=300)
+plt.show()
+
+
+# # Adaptive linear neurons and the convergence of learning
+
+# ...
+
+# ## Minimizing cost functions with gradient descent
+
+# ## Implementing an adaptive linear neuron in Python
+
+
+
+class AdalineGD(object):
+    """ADAptive LInear NEuron classifier.
+
+    Parameters
+    -----------
+    eta: float
+        Learning rate (between 0.0 and 1.0)
+    n_iter: int
+        Passes over the training dataset
+    random_state: int
+        Random number generator seed for random weight
+        initialization.
+
+
+    Attributes
+    -----------
+    w_: 1d-array
+        Weights after fitting
+    cost_: list
+        Sum of squares cost function value in each epoch
+    
+    """
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+        """ Fit training data
+
+        Parameters
+        -----------
+        X: {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of examples and
+            n_features is the number of features.
+        y: array-like, shape = [n_examples]
+            Target values.
+
+        Returns
+        --------
+        self:  object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            # Please note that the "activation" method has no effect
+            # in the code since it is simply an identity function. We
+            # could write `output = self.net_input(X)` directly instead. 
+            # The purpose of the activation is more conceptual, i.e.,
+            # in the case of logistic regression, we could change it o
+            # sigmoid function to implement a logistic regression classifier.
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+    
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def activation(self, X):
+        """Compute linear activation"""
+        return X
+    
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.0, 1, -1)
+
+
+
+
+
+
 
