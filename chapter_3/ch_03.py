5d923af34e9de3a5792683605c20f759e4d0da2e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-11 22:06:05 -0400

    Add files via upload

---
+++ chapter_3/ch_03.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import matplotlib
+from distutils.version import LooseVersion
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from pydotplus import graph_from_dot_data
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
