ca53943329cc2caefa8f447274a917d8ebfeea05
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-12 20:12:01 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -20,3 +20,18 @@ from pydotplus import graph_from_dot_data
 from sklearn.tree import export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
+
+
+
+# Choosing a classification algo
+
+# First steps with scikit-learn
+
+# Loading the Iris dataset from scikit-learn. Here, the third column represents the petal length, and the fourth column the petal width of the flower examples. The classes are already converted to integer labels where 0=Iris-Setosa, 1=Iris-Versicolor, 2=Iris-Virginica.
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+print('Class labels:', np.unique(y))
+
