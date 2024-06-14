aad52566ea90cfda53426f337c10c8eaed9d0140
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-13 20:54:25 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -120,3 +120,19 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                         marker='o',
                         s=100,
                         label='test set')
+
+# Training a perceptron model using the standardized training data:
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.savefig('03_01.png', dpi=300)
+plt.show()
+
