a6538ab12c037f174446bd8234f4f0a1f93827ed
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-15 16:47:15 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -133,6 +133,30 @@ plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 
 plt.tight_layout()
-plt.savefig('03_01.png', dpi=300)
+# plt.savefig('03_01.png', dpi=300)
 plt.show()
 
+# Modeling class probabilities via logistic regression
+
+# Logistic regression intuition and conditional probabilities
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+
+plt.plot(z, phi_z)
+plt.axvline(0, 0, color='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+
+# y axis ticks and gridline
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+
+plt.tight_layout()
+plt.savefig('images/03_02.png', dpi=300)
+plt.show()
