2bbe1eb4fbbd08f48aca7bf6a5f7cf0c2b8c2a65
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-16 16:15:45 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -188,3 +188,75 @@ plt.tight_layout()
 plt.savefig('03_04.png', dpi=300)
 plt.show()
 
+
+class LogisticRegression(object):
+    """Logistic Regression Classifier using gradient descent.
+
+    Parameters
+    -----------
+    eta: float
+        Learning Rate (between 0.0 and 1.0)
+    n_iter: int
+        Passes over the training dataset
+    random_state: int
+        Random number generator seed for random weight
+        initialization
+
+    Attributes
+    -----------
+    w_: 1d-array
+        Weights after fitting
+    cost_: list
+        Logistic cost function value in each epoch
+    
+    """
+
+    def __init__(self, eta=0.5, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data
+
+        Parameters
+        -----------
+        X: {array-like}, shape = [n_examples, n_features]
+            Training vectors, where n_examples is the number of examples and
+            n_features is the number of features
+        y: array-like, shape = [n_examples]
+            Target values
+
+        Returns
+        --------
+        self: object
+        
+        """
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+
+            # note that we compute the logistic `cost` now
+            # instand of the sum of squred errors cost
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
