4d0485851465d2fa784db7181f08cfb21753f5d2
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-16 19:25:04 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -283,6 +283,22 @@ plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 
 plt.tight_layout()
-plt.savefig('03_05.png', dpi=300)
+# plt.savefig('03_05.png', dpi=300)
 plt.show()
+
+# Training a logistic regression model with scikit-learn
+
+lr = LogisticRegression(C=100.0, random_state=1, solver='lbfgs', multi_class='ovr')
+lr.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=lr, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')                      
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('03_06.png', dpi=300)
+plt.show()
+
 
