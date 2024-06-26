d386e17434e5f747b7ae5c87e1ad0df1aef574d0
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-25 23:06:19 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -332,8 +332,27 @@ plt.ylabel('weight coefficient')
 plt.xlabel('C')
 plt.legend(loc='upper left')
 plt.xscale('log')
-plt.savefig('03_08.png', dpi=300)
+# plt.savefig('03_08.png', dpi=300)
 plt.show()
 
 
-    
+# Maximum margin classification with support vector machines
+
+# Maximum margin intuition
+
+# Dealing with the nonlinearly separable case using slack variables
+
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std,
+                      y_combined,
+                      classifier=svm,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('03_11.png', dpi=300)
+plt.show()
+
