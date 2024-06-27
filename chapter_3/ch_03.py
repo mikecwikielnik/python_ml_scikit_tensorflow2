de7a1b5829c39b8fadd72260657f26121583f448
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-27 15:53:25 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -362,3 +362,29 @@ ppn = SGDClassifier(loss = 'perceptron')
 lr = SGDClassifier(loss = 'log')
 svm = SGDClassifier(loss = 'hinge')
 
+# Solving non-linear problems using a kernel SVM
+
+np.random.seed(1)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                       X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(X_xor[y_xor == 1, 0],
+            X_xor[y_xor == 1, 1],
+            c = 'b', marker='x',
+            label='1')
+plt.scatter(X_xor[y_xor == -1, 0],
+            X_xor[y_xor == -1, 1],
+            c = 'r',
+            marker = 's',
+            label = '-1')
+
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.savefig('03_12.png', dpi=300)
+plt.show()
+
+
