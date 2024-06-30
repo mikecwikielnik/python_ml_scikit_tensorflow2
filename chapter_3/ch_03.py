f8c70d975953116fbca008f671bbabc395468dd3
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-30 00:36:35 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -392,6 +392,7 @@ plt.show()
 
 svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
 svm.fit(X_xor, y_xor)
+
 plot_decision_regions(X_xor, y_xor,
                       classifier=svm)
 plt.legend(loc='upper left')
@@ -411,5 +412,25 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
-plt.savefig('03_15.png', dpi=300)
+# plt.savefig('03_15.png', dpi=300)
 plt.show()
+
+
+
+
+svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('03_16.png', dpi=300)
+plt.show()
+
+
+
+# Decision tree learning
+
