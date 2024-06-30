e82a78f70d834611fd49ee27ad82a055f9b44689
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-06-30 00:27:23 -0400

    Add files via upload

--- chapter_3/ch_03.py
+++ chapter_3/ch_03.py
@@ -396,6 +396,20 @@ plot_decision_regions(X_xor, y_xor,
                       classifier=svm)
 plt.legend(loc='upper left')
 plt.tight_layout()
-plt.savefig('03_14.png', dpi=300)
+# plt.savefig('03_14.png', dpi=300)
 plt.show()
 
+
+
+
+svm = SVC(kernel='rbf', random_state=1, gamma=0.2, C=1.0)
+svm.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined,
+                      classifier=svm, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.savefig('03_15.png', dpi=300)
+plt.show()
