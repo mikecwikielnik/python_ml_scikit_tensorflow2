4f1199731bb77768b83083a86ee7cf10350c39aa
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-06 19:53:05 -0400

    Add files via upload

---
+++ chapter_4/ch_04.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+
+import pandas as pd
+from io import StringIO
+import sys
+from sklearn.impute import SimpleImputer
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from sklearn.base import clone
+from itertools import combinations
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+# Dealing with missing data
+
+
+
