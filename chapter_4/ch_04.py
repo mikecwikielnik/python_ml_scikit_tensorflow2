9d8f4e85daf4d6e43bbd500d323fe8a0d995ea44
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-06 20:19:20 -0400

    Add files via upload

--- chapter_4/ch_04.py
+++ chapter_4/ch_04.py
@@ -25,9 +25,10 @@ from sklearn.feature_selection import SelectFromModel
 
 csv_data = '''A,B,C,D
 1.0, 2.0, 3.0, 4.0
-5.0, 6.0, , 8.0
-10.0, 11.0, 12.0, 
-'''
+5.0, 6.0,, 8.0
+10.0, 11.0, 12.0,'''
 
 df = pd.read_csv(StringIO(csv_data))
 df
+
+df.isnull().sum()
