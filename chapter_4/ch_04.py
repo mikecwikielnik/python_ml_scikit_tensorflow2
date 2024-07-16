bac0973afc0a439d9bed001f29ceed3afbd63431
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-15 23:31:05 -0400

    Add files via upload

--- chapter_4/ch_04.py
+++ chapter_4/ch_04.py
@@ -274,4 +274,97 @@ print('Test accuracy', lr.score(X_test_std, y_test))
 lr.intercept_
 
 
-lr.coef_
+lr.coef_   
+
+fig = plt.figure()
+ax = plt.subplot()
+
+colors = ['blue', 'green', 'red', 'cyan',
+          'magenta', 'yellow', 'black',
+          'pink', 'lightgreen', 'lightblue',
+          'gray', 'indigo', 'orange']
+
+weights, params = [], []
+for c in np.arange(-4., 6.):
+    lr = LogisticRegression(penalty='l1', C=10.**c, solver='liblinear',
+                            multi_class='ovr', random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column + 1],
+             color=color)
+
+plt.axhline(0, color='black', linestyle='--', linewidth=3)    
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+
+# plt.savefig('04_07.png', dpi=300,
+            # bbox_inches='tight', pad_inches=0.2)
+plt.show()            
+
+
+
+# Sequential feature selection algo
+
+
+class SBS():
+    def __init__(self, estimator, k_features, scoring=accuracy_score,
+                test_size=0.25, random_state=1):
+        self.scoring = scoring
+        self.estimator = clone(estimator)
+        self.k_features = k_features
+        self.test_size = test_size
+        self.random_state = random_state
+
+    def fit(self, X, y):
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = self.test_size,
+                            random_state = self.random_state)
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        score = self._calc_score(X_train, y_train,
+                                X_test, y_test, self.indices_)
+        self.scores_ = [score]
+
+        while dim > self.k_features:
+            scores = []
+            subsets = []
+
+            for p in combinations(self.indices_, r = dim - 1):
+                score = self._calc_score(X_train, y_train,
+                                        X_test, y_test, p)
+                scores.append(score)                                    
+                subsets.append(p)
+
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            dim -= 1
+
+            self.scores_.append(scores[best])
+        self.k_score_ = self.scores_[-1]
+
+        return self
+
+    def transform(self, X):
+        return X[:, self.indices_]
+
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+
+
