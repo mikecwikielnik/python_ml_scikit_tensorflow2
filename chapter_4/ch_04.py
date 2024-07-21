98c702e3e2e20b2747672ad9ce31e2c603167cf2
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-21 16:21:54 -0400

    Add files via upload

--- chapter_4/ch_04.py
+++ chapter_4/ch_04.py
@@ -374,3 +374,16 @@ knn = KNeighborsClassifier(n_neighbors = 5)
 sbs = SBS(knn, k_features=1)
 sbs.fit(X_train_std, y_train)
 
+# plotting performance of feature subsets
+
+k_feat = [len(k) for k in sbs.subsets_]
+
+plt.plot(k_feat, sbs.scores_, marker = 'o')
+plt.ylim([0.7, 1.02])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.tight_layout()
+# plt.savefig('04_08.png', dpi=300)
+plt.show()
+
