07ae9a3b14de5e90ac2bffd6ad0dddc4e132d5bd
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-25 11:12:14 -0400

    Add files via upload

--- chapter_5/ch_05.py
+++ chapter_5/ch_05.py
@@ -31,7 +31,24 @@ df_wine = pd.read_csv('wine.data', header=None)
 
 df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
                    'Alcalinity of ash', 'Magnesium', 'Total phenols',
-                   ]
+                   'Flavanoids', 'Nonflavanoid phenols', 'Proanthocyanis',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines', 'Proline']
 
 df_wine.head()
+
+# Splitting the data into 70% training and 30% test subsets
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+                                                    stratify=y,
+                                                    random_state=0)
+
+
+# Standardizing the data
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
 
