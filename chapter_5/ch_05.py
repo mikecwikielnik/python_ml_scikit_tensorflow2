b10bfd272768b8c42251fca172d30d323c0dbc1f
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-25 23:34:23 -0400

    Add files via upload

--- chapter_5/ch_05.py
+++ chapter_5/ch_05.py
@@ -60,3 +60,22 @@ eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 
 print('\nEigenvalues \n%s' % eigen_vals)
 
+
+# Total and explained variance
+
+tot = sum(eigen_vals)
+var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+cum_var_exp = np.cumsum(var_exp)
+
+
+plt.bar(range(1, 14), var_exp, alpha=0.5, align='center',
+        label = 'Individual explained variance')
+plt.step(range(1, 14), cum_var_exp, where='mid',
+         label = 'Cumulative explained variance')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal component index')
+plt.legend(loc = 'best')
+plt.tight_layout()
+# plt.savefig('05_02.png', dpi=300)
+plt.show()
+
