610ded8382e0883895c500b4e93a47fd0227ac4e
Mike <79020525+mikecwikielnik@users.noreply.github.com>
2024-07-27 23:41:57 -0400

    Add files via upload

--- chapter_5/ch_05.py
+++ chapter_5/ch_05.py
@@ -249,3 +249,21 @@ for label, mv in zip(range(1, 4), mean_vecs):
 
 print('Within-class scatter matrix: %sx%s' % (S_W.shape[0], S_W.shape[1]))
 
+
+
+# Better: covariance matrix since classes are not equally distributed:
+
+
+print('Class label distribution: %s'
+      % np.bincount(y_train)[1:])
+
+
+d = 13 # number of features
+S_W = np.zeros((d, d))
+for label, mv in zip(range(1, 4), mean_vecs):
+        class_scatter = np.cov(X_train_std[y_train == label].T)
+        S_W += class_scatter
+print('Scaled within-class scatter matrix: %sx%s' % (S_W.shape[0],
+                                                     S_W.shape[1]))
+
+
